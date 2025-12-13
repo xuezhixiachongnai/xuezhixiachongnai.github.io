0dfe4294483ba8acca71079da2202dc3f53757e4
hj <2978224828@qq.com>
2025-12-14 02:00:44 +0800

    remove public directory

--- public/js/background.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const images = [
-    "/images/background-image/img1.jpg",
-    "/images/background-image/img2.jpg",
-    "/images/background-image/img3.jpg"
-  ];
-  const bg = images[Math.floor(Math.random() * images.length)];
-  document.body.style.backgroundImage = `url('${bg}')`;
-})();
